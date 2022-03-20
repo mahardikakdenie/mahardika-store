b9a2910f9c46d1ba11915f1bdf4f7768e8d3478f
MahardikaKDenie <dikamahar884@gmail.com>
2022-03-21 03:48:45 +0700

    push

--- Front-end/src/components/Hero.vue
+++ Front-end/src/components/Hero.vue
@@ -2,7 +2,13 @@
   <!-- Hero Section Begin -->
   <section class="hero-section">
     <!-- <div class=" owl-carousel"></div> -->
-    <carousel class="hero-items" :items="1" :nav="false" :autoplay="true">
+    <carousel
+      class="hero-items"
+      :dots="false"
+      :items="1"
+      :nav="false"
+      :autoplay="true"
+    >
       <!-- <template slot="prev"><span class="prev">prev</span></template> -->
       <div
         class="single-hero-items set-bg"
@@ -40,24 +46,7 @@
           </div>
         </div>
       </div>
-      <div
-        class="single-hero-items set-bg"
-        style="background-image: url('img/hero-3.jpg')"
-      >
-        <div class="container">
-          <div class="row">
-            <div class="col-lg-5">
-              <span>Bag,kids</span>
-              <h1>Black friday</h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore
-              </p>
-              <a href="#" class="primary-btn">Shop Now</a>
-            </div>
-          </div>
-        </div>
-      </div>
+
       <!-- <template slot="next"><span class="next">next</span></template> -->
     </carousel>
   </section>
@@ -72,5 +61,3 @@ export default {
   },
 };
 </script>
-
-<style></style>
